8b3d2f8cb3c142f2ed3dcc1224e32173b69a7252
Samyak Thapa <thapasamyak@gmail.com>
2024-01-09 18:14:01 -0500

    Basic website template

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind(), mdx(), image()],
+	site: 'https://samyakthapa.github.io',
+	// base: '/my-repo',
+});
